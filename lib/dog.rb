4033d6dfd63066e07c949b51ebef76a0ed5989a2
Aaron Summers <aaron.summers2797@gmail.com>
2019-05-16 13:54:50 +0100

    attempting to define .all method

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Dog
   attr_accessor :name
   @@all = []
@@ -5,5 +6,16 @@ class Dog
   def initialize(dog_name)
     @name = dog_name
     @@all << @name
+
+    #binding.pry
   end
+
+  def self.all
+    puts @@all
+    #binding.pry
+  end
+
+  # def self.clear_all
+  #   @@all.delete
+  # end
 end
