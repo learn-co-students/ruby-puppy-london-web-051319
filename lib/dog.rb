bfe888e3cc2b4e9ece74f90780837a98c9e3cfa9
Rudi Hinds <rudihinds@gmail.com>
2019-05-17 18:08:05 +0100

    puppies

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+
+attr_accessor :name
+
+@@all = []
+
+def self.clear_all
+  @@all.clear
+end
+
+def self.all
+  @@all.each do |item|
+    puts item.name
+  end
+end
+
+def initialize(name)
+  @name = name
+  @@all << self
+
+end
+
+end
